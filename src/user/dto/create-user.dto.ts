fceb92b65d464122cb6edb931487acc08e1b44f8
douglasbarros <douglas.silva@genesis.global>
2025-01-13 17:32:00 -0300

    new implementations.

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsDateString,
+  IsEmail,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword({
+    minLength: 6,
+  })
+  password: string;
+
+  @IsOptional()
+  @IsDateString()
+  birthAt: string;
+}
